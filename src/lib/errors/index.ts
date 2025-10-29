997ce3ecbabc3f519a99743d6f762a458d049bc8
dimas-fahmi <dimasfahmipebrian@gmail.com>
2025-10-30 01:34:37 +0700

    chore(project): create standardized error

---
+++ src/lib/errors/index.ts
@@ -0,0 +1,31 @@
+// HTTP Status
+export type HttpStatus = 200 | 400 | 401 | 403 | 404 | 500;
+
+// Error Codes
+export type ErrorCode = "failed_insertion" | "failed_mutation" | "not_found";
+
+export interface StandardizedErrorType {
+  code: ErrorCode;
+  message: string;
+  status: HttpStatus;
+  context?: unknown;
+}
+
+export class StandardizedError extends Error {
+  code: ErrorCode;
+  status?: HttpStatus;
+  context?: unknown;
+
+  constructor(
+    code: ErrorCode,
+    message: string,
+    status?: HttpStatus,
+    context?: unknown
+  ) {
+    super(message);
+    this.name = "StandardizedError";
+    this.code = code;
+    this.status = status || 500;
+    this.context = context;
+  }
+}
