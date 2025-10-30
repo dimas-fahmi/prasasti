adb04f8b0ed66271742d4459fd962528ae6735c3
dimas-fahmi <dimasfahmipebrian@gmail.com>
2025-10-30 13:59:25 +0700

    scaffold(landing): finalize hero

--- app/sections/HeroSection.tsx
+++ app/sections/HeroSection.tsx
@@ -11,66 +11,71 @@ const LandingHeroSection = () => {
   const router = useRouter();
 
   return (
-    <section className="min-h-dvh p-4 md:p-16 lg:p-24 max-w-7xl mx-auto flex items-center justify-between">
+    <section className="relative min-h-screen p-4 md:p-16 lg:p-24 max-h-screen flex flex-col-reverse justify-end max-w-7xl mx-auto md:grid md:grid-cols-2">
       {/* Content */}
-      <div className="max-w-md md:pb-16">
-        {/* Logo */}
-        <Image
-          width={180}
-          height={40}
-          src={"/assets/logos/svg/horizontal.svg"}
-          alt="Prasasti Logo"
-          className="mb-6 hidden md:block"
-        />
+      <div className="md:flex md:items-center">
+        <div className="max-w-md md:pb-16 text-center md:text-left flex flex-col justify-between h-full flex-1 pb-6 gap-4 md:gap-2">
+          {/* Logo */}
+          <div className="flex flex-col gap-4 md:gap-2">
+            <Image
+              width={180}
+              height={40}
+              src={"/assets/logos/svg/horizontal.svg"}
+              alt="Prasasti Logo"
+              className="mb-6 hidden md:block"
+            />
 
-        <h1 className="font-header font-bold text-4xl mb-4">
-          Write. Preserve. Own Forever.
-        </h1>
-        <p className="text-sm font-light">
-          Prasasti is an offline-first note app designed for permanence and
-          privacy. Your ideas live inside your device — never on a server.
-          Create Artifacts, inscribe your thoughts, and keep them safe in your
-          own Prasasti. Download, import, and preserve your knowledge — just
-          like an ancient inscription carved in stone.
-        </p>
+            <h1 className="font-header font-bold text-4xl mb-4">
+              Write. Preserve. Own Forever.
+            </h1>
+            <p className="md:text-sm font-light">
+              Prasasti is an offline-first note app designed for permanence and
+              privacy. Your ideas live inside your device — never on a server.
+              Create Artifacts, inscribe your thoughts, and keep them safe in
+              your own Prasasti. Download, import, and preserve your knowledge —
+              just like an ancient inscription carved in stone.
+            </p>
+          </div>
 
-        <div className="mt-6 flex gap-2">
-          <Button
-            className="group/button"
-            size={"lg"}
-            disabled={isLoadingMetadata}
-            onClick={() => {
-              if (isLoadingMetadata) return;
+          <div className="mt-6 md:flex gap-2 grid grid-cols-2 ">
+            <Button
+              className="group/button"
+              size={"lg"}
+              disabled={isLoadingMetadata}
+              onClick={() => {
+                if (isLoadingMetadata) return;
 
-              if (metadata) {
-                router.push("/dashboard");
-              } else {
-                router.push("/new");
-              }
-            }}
-          >
-            <Lock className="group-hover/button:block hidden" />
-            <LockOpen className="group-hover/button:hidden block" />
-            Continue
-          </Button>
-          <Button
-            variant={"outline"}
-            size={"lg"}
-            disabled={isLoadingMetadata || !!metadata}
-          >
-            <Import />
-            Import Prasasti
-          </Button>
+                if (metadata) {
+                  router.push("/dashboard");
+                } else {
+                  router.push("/new");
+                }
+              }}
+            >
+              <Lock className="group-hover/button:block hidden" />
+              <LockOpen className="group-hover/button:hidden block" />
+              Continue
+            </Button>
+            <Button
+              variant={"outline"}
+              size={"lg"}
+              disabled={isLoadingMetadata || !!metadata}
+            >
+              <Import />
+              Import Prasasti
+            </Button>
+          </div>
         </div>
       </div>
 
       {/* Illustration */}
-      <div>
+      <div className="flex items-center justify-center md:justify-end flex-1">
         <Image
-          width={380}
-          height={380}
+          width={280}
+          height={280}
           src={"/assets/arts/svg/hero-art-01.svg"}
           alt="Inscription Art"
+          className="invert md:min-w-[380px] md:max-w-[380px]"
         />
       </div>
     </section>
