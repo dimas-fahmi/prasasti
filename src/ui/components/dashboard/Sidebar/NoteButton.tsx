f0ad4e5604f5260d0a709a98c291aa3e1342fedd
dimas-fahmi <dimasfahmipebrian@gmail.com>
2025-10-30 22:04:50 +0700

    style: redesign note button
    Truncate title if it's too long relative to container width.
    
    Add tooltip for note title

--- src/ui/components/dashboard/Sidebar/NoteButton.tsx
+++ src/ui/components/dashboard/Sidebar/NoteButton.tsx
@@ -1,19 +1,40 @@
+"use client";
+
 import { Note } from "@/src/db/idb/schema/note";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from "@/src/ui/shadcn/components/ui/tooltip";
 import { cn } from "@/src/ui/shadcn/lib/utils";
 import { Notebook } from "lucide-react";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const NoteButton = ({ note }: { note: Note }) => {
+  const pathname = usePathname();
+  const href = `/dashboard/artifacts/${note?.id}`;
+  const isActive = pathname === href;
+
   return (
-    <Link
-      href={`/dashboard/artifacts/${note?.id}`}
-      type="button"
-      className={cn(
-        `flex items-center gap-2 text-sm p-2 rounded-md w-full opacity-70 hover:bg-muted`
-      )}
-    >
-      <Notebook className="w-4 h-4" /> {note?.title || "Untitled Note"}
-    </Link>
+    <Tooltip delayDuration={1000}>
+      <TooltipTrigger asChild>
+        <Link
+          href={href}
+          type="button"
+          className={cn(
+            `flex items-center gap-2 text-sm p-2 rounded-md w-full`,
+            isActive ? "bg-muted" : "hover:bg-muted opacity-70 "
+          )}
+        >
+          <Notebook className="min-w-4 max-w-4 min-h-4 max-h-4" />{" "}
+          <span className="truncate text-ellipsis w-full">
+            {note?.title || "Untitled Note"}
+          </span>
+        </Link>
+      </TooltipTrigger>
+      {note?.title && <TooltipContent>{note.title}</TooltipContent>}
+    </Tooltip>
   );
 };
 
